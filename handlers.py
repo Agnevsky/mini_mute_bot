0d9678d1199eb4d155b26babdd1291b4c67b7889
thorenda <thorenda@student.21-school.ru>
2026-01-15 19:32:19 +0300

    внес некоторые изменения

--- handlers.py
+++ handlers.py
@@ -1,3 +1,5 @@
+import os, json
+from dotenv import load_dotenv
 from aiogram import Router, types, Bot
 from datetime import timedelta
 from aiogram import F
@@ -6,6 +8,9 @@ from aiogram.filters import CommandStart
 from datetime import datetime
 
 router = Router()
+
+load_dotenv()
+my_list = json.loads(os.getenv("ADMIN_ID"))
 
 @router.message(CommandStart())
 async def say_hello(message: Message):
@@ -17,22 +22,22 @@ async def say_hello(message: Message):
 async def get_id_user_for_muted(message: Message, bot: Bot):
     mute_command_list = message.text.split(' ') 
     if message.text.split(' ')[0] == '!mute' and len(mute_command_list) >= 1: 
-       
-        minutes = int(mute_command_list[1])
-        user_id = message.reply_to_message.from_user.id
-        name = message.reply_to_message.from_user.full_name
-        until_date = datetime.now() + timedelta(minutes=minutes)
+        if my_list[0] != '118642329':
+            minutes = int(mute_command_list[1])
+            user_id = message.reply_to_message.from_user.id
+            name = message.reply_to_message.from_user.full_name
+            until_date = datetime.now() + timedelta(minutes=minutes)
 
 
-        # используем message.bot, чтобы не зависеть от DI
-        await bot.restrict_chat_member(
-            chat_id=message.chat.id,
-            user_id=user_id,
-            permissions=types.ChatPermissions(can_send_messages=False),
-            until_date=until_date
-        )
+            # используем message.bot, чтобы не зависеть от DI
+            await bot.restrict_chat_member(
+                chat_id=message.chat.id,
+                user_id=user_id,
+                permissions=types.ChatPermissions(can_send_messages=False),
+                until_date=until_date
+            )
 
-        await message.answer(f"🔇 Пользователь {name} замучен на {minutes} минут")
-        print(f"Trying to mute user {user_id} in chat {message.chat.id} until {until_date}")
+            await message.answer(f"🔇 Пользователь {name} замучен на {minutes} минут")
 
-
+        else:
+            await message.answer(f"Ага соси приколист! ")
