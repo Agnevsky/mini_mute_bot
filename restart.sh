ffb8fa89f86b7cd2ce98623680488723352e474c
root <root@agnevsky.com>
2026-03-24 21:40:57 +0300

    внес некоторые изменения

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker-compose down
-docker-compose up -d --build
-docker-compose exec bot alembic upgrade head
-docker-compose logs -f
+docker compose down
+docker compose up -d --build
+export $(cat .env.local | grep -v ^# | xargs) && alembic upgrade head
+docker compose logs -f
